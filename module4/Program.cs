acca1b8b4c42640a4a602267ecdc22a36cc360fc
DenisValuev <valuev.denis91@mail.ru>
2023-11-14 15:09:04 +0300

    Добавьте файлы проекта.

---
+++ module4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace module4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Denis";
+            Console.WriteLine(MyName);
+        }
+    }
+}
