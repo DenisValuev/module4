ad61e753cb8f1279c8dd9bb6bda4b8211ec2aecb
DenisValuev <valuev.denis91@mail.ru>
2023-11-14 16:02:28 +0300

    module4-master: добавлен вывод на экран логических литералов

--- module4/Program.cs
+++ module4/Program.cs
@@ -10,7 +10,11 @@
             Console.WriteLine("\t Привет, мир"); //вывод литералов
             Console.WriteLine("\t Мне 27 лет");
             Console.WriteLine("\t My name is \n Jane");
-            Console.WriteLine("\x01");
+
+            Console.WriteLine("\x01");// вывод символьного литерала
+
+            Console.WriteLine(true); // вывод логических литералов
+            Console.WriteLine(false);
             Console.ReadKey();
         }
     }
